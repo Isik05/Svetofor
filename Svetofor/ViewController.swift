ff396b099b61f194b7d4da902f5206315ef03048
Islam <islam_mirzahanov@mail.ru>
2022-06-19 16:13:02 +0300

    create traffic lights

--- Svetofor/ViewController.swift
+++ Svetofor/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redcolorView: UIView!
+    @IBOutlet var yellowcolorView: UIView!
+    @IBOutlet var greencolorView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redcolorView.layer.cornerRadius = 50
+        redcolorView.backgroundColor = UIColor.red.withAlphaComponent(0.25)
+        
+        yellowcolorView.layer.cornerRadius = 50
+        yellowcolorView.backgroundColor = UIColor.yellow.withAlphaComponent(0.25)
+        
+        greencolorView.layer.cornerRadius = 50
+        greencolorView.backgroundColor = UIColor.green.withAlphaComponent(0.25)
+        
+        startButton.tintColor = UIColor.red
     }
 
-
+    @IBAction func startAppButton() {
+        
+    }
+    
 }
 
