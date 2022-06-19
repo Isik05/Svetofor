66ff6f9f017b0280ff4a69eeada3382af0f02a9e
Islam <islam_mirzahanov@mail.ru>
2022-06-19 22:09:53 +0300

    1

--- Svetofor/ViewController.swift
+++ Svetofor/ViewController.swift
@@ -18,19 +18,37 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         redcolorView.layer.cornerRadius = 50
         redcolorView.backgroundColor = UIColor.red.withAlphaComponent(0.25)
-        
+
         yellowcolorView.layer.cornerRadius = 50
         yellowcolorView.backgroundColor = UIColor.yellow.withAlphaComponent(0.25)
         
         greencolorView.layer.cornerRadius = 50
         greencolorView.backgroundColor = UIColor.green.withAlphaComponent(0.25)
         
-        startButton.tintColor = UIColor.red
+        startButton.tintColor = UIColor.blue
     }
 
-    @IBAction func startAppButton() {
-        
+    @IBAction func startAppButton(_ sender: UIButton) {
+        switch UIColor() {
+        case .red:
+            redcolorView.backgroundColor = UIColor.red.withAlphaComponent(1)
+        case .yellow:
+            yellowcolorView.backgroundColor = UIColor.yellow.withAlphaComponent(1)
+        case .green:
+            greencolorView.backgroundColor = UIColor.green.withAlphaComponent(1)
+        default:
+            print("error")
+        }
     }
-    
+
 }
 
+//        switch UIButton(){
+//        case redcolorView:
+//            redcolorView.backgroundColor = UIColor.red.withAlphaComponent(1)
+//        case yellowcolorView:
+//            yellowcolorView.backgroundColor = UIColor.yellow.withAlphaComponent(1)
+//        case greencolorView:
+//            greencolorView.backgroundColor = UIColor.green.withAlphaComponent(1)
+//        default:
+//            print("error")
